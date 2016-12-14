6a28f72793ec27146f32e0195abd5220b0b8b7fc
aspanjer <aspanjer@uw.edu>
2016-12-14 09:18:36 -0800

    GO figures

--- analysis/GeneOntology/Jenkins/GO_barplot.R
+++ analysis/GeneOntology/Jenkins/GO_barplot.R
@@ -1,90 +1,46 @@
-##Barplots for REVIGO molecular function
-#import files and reduce down to top 20 by frequency
-mf.IS<-read.csv(file="REVIGOmf.csv", header=T)
-mf.IS.order<-mf.IS[order(mf.IS$frequency, decreasing= TRUE),]
-mf.IS.order.20<-mf.IS.order[c(1:15),]
-mf.IS.order.20<-mf.IS.order.20[order(mf.IS.order.20$frequency, decreasing= FALSE),]
+####GO Enrichment###
 
-
-#barplot for revigo condensed terms 
-par(mfrow=c(3,1), oma=c(4,15,4,0), mar=c(.5,.5,.5,1))
-barplot(mf.IS.order.20$frequency, 
-        names.arg = mf.IS.order.20$description, 
-        horiz=TRUE, las=1, cex.names=.7, xaxt="n")
-mtext("Molecular Function", side=3, line=-10)
-
-##Barplots for REVIGO molecular function
-#import files and reduce down to top 20 by frequency
-cc.IS<-read.csv(file="REVIGOcc.csv", header=T)
-cc.IS.order<-cc.IS[order(cc.IS$frequency, decreasing= TRUE),]
-cc.IS.order.20<-cc.IS.order[c(1:15),]
-cc.IS.order.20<-cc.IS.order.20[order(cc.IS.order.20$frequency, decreasing= FALSE),]
-
-
-#barplot for revigo condensed terms 
-barplot(cc.IS.order.20$frequency, 
-        names.arg = cc.IS.order.20$description, 
-        horiz=TRUE, las=1, cex.names=.7, xaxt="n")
-mtext("Cellular Component", side=3, line=-10)
-
-##Barplots for REVIGO molecular function
-#import files and reduce down to top 20 by frequency
-bp.IS<-read.csv(file="REVIGObp.csv", header=T)
-bp.IS.order<-bp.IS[order(bp.IS$frequency, decreasing= TRUE),]
-bp.IS.order.20<-bp.IS.order[c(1:15),]
-bp.IS.order.20<-bp.IS.order.20[order(bp.IS.order.20$frequency, decreasing= FALSE),]
-
-#barplot for revigo condensed terms 
-barplot(bp.IS.order.20$frequency, 
-        names.arg = bp.IS.order.20$description, 
-        horiz=TRUE, las=1, cex.names=.7, ylab="frequency of GO term")
-mtext("Biological Process", side=3, line=-10)
-mtext("frequency of GO term", side=1, outer=TRUE, line=2)
-mtext("A) Issaquah v Coulter", outer=TRUE, adj=0 )
-
-
-####GO Enrichment
-
-mf.IS.enrich<-read.delim(file="BP_Issaquah.txt", header=T)
-mf.IS.order.enrich<-mf.IS.enrich[order(mf.IS.enrich$PValue),]
-mf.IS.order.enrich.20<-mf.IS.enrich[c(1:15),]
-mf.IS.order.enrich.20<-mf.IS.order.enrich.20[order(mf.IS.order.enrich.20$Count, decreasing= FALSE),]
+mf.JE.enrich<-read.delim(file="MF_Jenkins.txt", header=T)
+mf.JE.order.enrich<-mf.JE.enrich[order(mf.JE.enrich$PValue),]
+mf.JE.order.enrich.20<-mf.JE.enrich[c(1:15),]
+mf.JE.order.enrich.20<-mf.JE.order.enrich.20[order(mf.JE.order.enrich.20$Count, decreasing= FALSE),]
 
 
 #barplot for erichment molecular function
-par(mfrow=c(3,1), oma=c(4,18,4,0), mar=c(.5,.5,.5,1))
-barplot(mf.IS.order.enrich.20$Count, 
-        names.arg = mf.IS.order.enrich.20$GO_Term, 
-        horiz=TRUE, las=1, cex.names=.7, xaxt="n")
-mtext("Molecular Function", side=3, line=-10)
+par(mfrow=c(3,1), oma=c(4,21,4,0), mar=c(.5,.5,.5,1))
+barplot(mf.JE.order.enrich.20$Count, 
+        names.arg = mf.JE.order.enrich.20$GO_Term, 
+        horiz=TRUE, las=1, cex.names=1, xaxt="n")
+mtext("Molecular Function", side=3, line=-14)
 
 #cc
-cc.IS.enrich<-read.delim(file="CC_Issaquah.txt", header=T)
-cc.IS.order.enrich<-cc.IS.enrich[order(cc.IS.enrich$PValue),]
-cc.IS.order.enrich.20<-cc.IS.enrich[c(1:15),]
-cc.IS.order.enrich.20<-cc.IS.order.enrich.20[order(mf.IS.order.enrich.20$Count, decreasing= FALSE),]
+cc.JE.enrich<-read.delim(file="CC_Jenkins.txt", header=T)
+cc.JE.order.enrich<-cc.JE.enrich[order(cc.JE.enrich$PValue),]
+cc.JE.order.enrich.20<-cc.JE.enrich[c(1:15),]
+cc.JE.order.enrich.20<-cc.JE.order.enrich.20[order(cc.JE.order.enrich.20$Count, decreasing=FALSE),]
 
 
 #barplot for erichment
-barplot(mf.IS.order.enrich.20$Count, 
-        names.arg = mf.IS.order.enrich.20$GO_Term, 
-        horiz=TRUE, las=1, cex.names=.7, xaxt="n")
-mtext("Cellular Component", side=3, line=-10)
+barplot(cc.JE.order.enrich.20$Count, 
+        names.arg = cc.JE.order.enrich.20$GO_Term, 
+        horiz=TRUE, las=1, cex.names=1, xaxt="n")
+mtext("Cellular Component", side=3, line=-14.5)
 
-bp.IS.enrich<-read.delim(file="BP_Issaquah.txt", header=T)
-bp.IS.order.enrich<-bp.IS.enrich[order(bp.IS.enrich$PValue),]
-bp.IS.order.enrich.20<-bp.IS.enrich[c(1:15),]
-bp.IS.order.enrich.20<-bp.IS.order.enrich.20[order(bp.IS.order.enrich.20$Count, decreasing= FALSE),]
+#bp
+bp.JE.enrich<-read.delim(file="BP_Jenkins.txt", header=T)
+bp.JE.order.enrich<-bp.JE.enrich[order(bp.JE.enrich$PValue),]
+bp.JE.order.enrich.20<-bp.JE.enrich[c(1:15),]
+bp.JE.order.enrich.20<-bp.JE.order.enrich.20[order(bp.JE.order.enrich.20$Count, decreasing= FALSE),]
 
 
 #barplot for erichment
-barplot(bp.IS.order.enrich.20$Count, 
-        names.arg = bp.IS.order.enrich.20$GO_Term, 
-        horiz=TRUE, las=1, cex.names=.7)
-mtext("Biological Proccesst", side=3, line=-10)
-mtext("Number of genes belonging to GO term", side=1, outer=TRUE, line=2)
-mtext("A) Issaquah v Coulter", outer=TRUE, adj=0 )
-
+barplot(bp.JE.order.enrich.20$Count, 
+        names.arg = bp.JE.order.enrich.20$GO_Term, 
+        horiz=TRUE, las=1, cex.names=1)
+mtext("Biological Process", side=3, line=-14)
+mtext("Number of DEGs belonging to GO term", side=1, outer=TRUE, line=2, at=.435)
+mtext("B) Jenkins v Coulter", outer=TRUE,line=1, cex=1.5, adj=0, at=.03 )
+mtext("GO Term", outer=TRUE, line=-1, cex=1, adj=1, at=-.03)
 
 
 #barplot for revigo condensed terms (might be easier to write about)
--- analysis/GeneOntology/Swamp/GO_barplot.R
+++ analysis/GeneOntology/Swamp/GO_barplot.R
@@ -1,90 +1,34 @@
-##Barplots for REVIGO molecular function
-#import files and reduce down to top 20 by frequency
-mf.IS<-read.csv(file="REVIGOmf.csv", header=T)
-mf.IS.order<-mf.IS[order(mf.IS$frequency, decreasing= TRUE),]
-mf.IS.order.20<-mf.IS.order[c(1:15),]
-mf.IS.order.20<-mf.IS.order.20[order(mf.IS.order.20$frequency, decreasing= FALSE),]
-
-
-#barplot for revigo condensed terms 
-par(mfrow=c(3,1), oma=c(4,15,4,0), mar=c(.5,.5,.5,1))
-barplot(mf.IS.order.20$frequency, 
-        names.arg = mf.IS.order.20$description, 
-        horiz=TRUE, las=1, cex.names=.7, xaxt="n")
-mtext("Molecular Function", side=3, line=-10)
-
-##Barplots for REVIGO molecular function
-#import files and reduce down to top 20 by frequency
-cc.IS<-read.csv(file="REVIGOcc.csv", header=T)
-cc.IS.order<-cc.IS[order(cc.IS$frequency, decreasing= TRUE),]
-cc.IS.order.20<-cc.IS.order[c(1:15),]
-cc.IS.order.20<-cc.IS.order.20[order(cc.IS.order.20$frequency, decreasing= FALSE),]
-
-
-#barplot for revigo condensed terms 
-barplot(cc.IS.order.20$frequency, 
-        names.arg = cc.IS.order.20$description, 
-        horiz=TRUE, las=1, cex.names=.7, xaxt="n")
-mtext("Cellular Component", side=3, line=-10)
-
-##Barplots for REVIGO molecular function
-#import files and reduce down to top 20 by frequency
-bp.IS<-read.csv(file="REVIGObp.csv", header=T)
-bp.IS.order<-bp.IS[order(bp.IS$frequency, decreasing= TRUE),]
-bp.IS.order.20<-bp.IS.order[c(1:15),]
-bp.IS.order.20<-bp.IS.order.20[order(bp.IS.order.20$frequency, decreasing= FALSE),]
-
-#barplot for revigo condensed terms 
-barplot(bp.IS.order.20$frequency, 
-        names.arg = bp.IS.order.20$description, 
-        horiz=TRUE, las=1, cex.names=.7, ylab="frequency of GO term")
-mtext("Biological Process", side=3, line=-10)
-mtext("frequency of GO term", side=1, outer=TRUE, line=2)
-mtext("A) Issaquah v Coulter", outer=TRUE, adj=0 )
-
-
-####GO Enrichment
-
-mf.IS.enrich<-read.delim(file="BP_Issaquah.txt", header=T)
-mf.IS.order.enrich<-mf.IS.enrich[order(mf.IS.enrich$PValue),]
-mf.IS.order.enrich.20<-mf.IS.enrich[c(1:15),]
-mf.IS.order.enrich.20<-mf.IS.order.enrich.20[order(mf.IS.order.enrich.20$Count, decreasing= FALSE),]
-
-
+####GO Enrichment###
+mf.SW.enrich<-read.delim(file="MF_Swamp.txt", header=T)
+mf.SW.order.enrich<-mf.SW.enrich[order(mf.SW.enrich$PValue),]
+mf.SW.order.enrich.20<-mf.SW.enrich[c(1:15),]
+mf.SW.order.enrich.20<-mf.SW.order.enrich.20[order(mf.SW.order.enrich.20$Count, decreasing= FALSE),]
 #barplot for erichment molecular function
-par(mfrow=c(3,1), oma=c(4,18,4,0), mar=c(.5,.5,.5,1))
-barplot(mf.IS.order.enrich.20$Count, 
-        names.arg = mf.IS.order.enrich.20$GO_Term, 
-        horiz=TRUE, las=1, cex.names=.7, xaxt="n")
+par(mfrow=c(3,1), oma=c(4,22,4,0), mar=c(.5,.5,.5,1))
+barplot(mf.SW.order.enrich.20$Count,
+names.arg = mf.SW.order.enrich.20$GO_Term,
+horiz=TRUE, las=1, cex.names=1, xaxt="n")
 mtext("Molecular Function", side=3, line=-10)
-
 #cc
-cc.IS.enrich<-read.delim(file="CC_Issaquah.txt", header=T)
-cc.IS.order.enrich<-cc.IS.enrich[order(cc.IS.enrich$PValue),]
-cc.IS.order.enrich.20<-cc.IS.enrich[c(1:15),]
-cc.IS.order.enrich.20<-cc.IS.order.enrich.20[order(mf.IS.order.enrich.20$Count, decreasing= FALSE),]
-
-
+cc.SW.enrich<-read.delim(file="CC_Swamp.txt", header=T)
+cc.SW.order.enrich<-cc.SW.enrich[order(cc.SW.enrich$PValue),]
+cc.SW.order.enrich.20<-cc.SW.enrich[c(1:15),]
+cc.SW.order.enrich.20<-cc.SW.order.enrich.20[order(cc.SW.order.enrich.20$Count, decreasing= FALSE),]
 #barplot for erichment
-barplot(mf.IS.order.enrich.20$Count, 
-        names.arg = mf.IS.order.enrich.20$GO_Term, 
-        horiz=TRUE, las=1, cex.names=.7, xaxt="n")
+barplot(cc.SW.order.enrich.20$Count,
+names.arg = cc.SW.order.enrich.20$GO_Term,
+horiz=TRUE, las=1, cex.names=1, xaxt="n")
 mtext("Cellular Component", side=3, line=-10)
-
-bp.IS.enrich<-read.delim(file="BP_Issaquah.txt", header=T)
-bp.IS.order.enrich<-bp.IS.enrich[order(bp.IS.enrich$PValue),]
-bp.IS.order.enrich.20<-bp.IS.enrich[c(1:15),]
-bp.IS.order.enrich.20<-bp.IS.order.enrich.20[order(bp.IS.order.enrich.20$Count, decreasing= FALSE),]
-
-
+#bp
+bp.SW.enrich<-read.delim(file="BP_Swamp.txt", header=T)
+bp.SW.order.enrich<-bp.SW.enrich[order(bp.SW.enrich$PValue),]
+bp.SW.order.enrich.20<-bp.SW.enrich[c(1:15),]
+bp.SW.order.enrich.20<-bp.SW.order.enrich.20[order(bp.SW.order.enrich.20$Count, decreasing= FALSE),]
 #barplot for erichment
-barplot(bp.IS.order.enrich.20$Count, 
-        names.arg = bp.IS.order.enrich.20$GO_Term, 
-        horiz=TRUE, las=1, cex.names=.7)
-mtext("Biological Proccesst", side=3, line=-10)
-mtext("Number of genes belonging to GO term", side=1, outer=TRUE, line=2)
-mtext("A) Issaquah v Coulter", outer=TRUE, adj=0 )
-
-
-
-#barplot for revigo condensed terms (might be easier to write about)
+barplot(bp.SW.order.enrich.20$Count,
+names.arg = bp.SW.order.enrich.20$GO_Term,
+horiz=TRUE, las=1, cex.names=1)
+mtext("Biological Process", side=3, line=-10)
+mtext("Number of DEGs belonging to GO term", side=1, outer=TRUE, line=2, at=.435)
+mtext("C) Swamp v Coulter", outer=TRUE,line=1, cex=1.5, adj=0, at=.03 )
+mtext("GO Term", outer=TRUE, line=-1, cex=1, adj=1, at=-.03)
